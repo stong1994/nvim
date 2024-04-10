0af9ba84706417fbb76572263ae1a622b03cf938
stong <498327580@qq.com>
2024-04-10 10:02:49 +0800

    init

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,3 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocms that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,15 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+return {
+  -- add symbols-outline
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    opts = {
+      -- add your options that should be passed to the setup() function here
+      position = "right",
+    },
+  },
+}
