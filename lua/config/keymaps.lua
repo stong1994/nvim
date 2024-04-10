ca7a19a13f73252667c2a4ca392e470b8003a252
stong <498327580@qq.com>
2024-04-10 15:02:37 +0800

    symbols outline

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,15 +7,3 @@ vim.keymap.set(
   require("telescope.builtin").resume,
   { noremap = true, silent = true, desc = "Resume" }
 )
-return {
-  -- add symbols-outline
-  {
-    "simrat39/symbols-outline.nvim",
-    cmd = "SymbolsOutline",
-    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
-    opts = {
-      -- add your options that should be passed to the setup() function here
-      position = "right",
-    },
-  },
-}
