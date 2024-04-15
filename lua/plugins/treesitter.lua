51b2cc9d8c37ced253c622fdca32fe9f2794ddd3
stong <498327580@qq.com>
2024-04-15 18:01:39 +0800

    move in functions/classes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -74,23 +74,63 @@ return {
         "vimdoc",
         "xml",
         "yaml",
+        -- "go",
       },
+      -- something copied from https://github.com/ray-x/go.nvim?tab=readme-ov-file#text-object
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
+          -- mappings for incremental selection (visual mappings)
+          init_selection = "gnn", -- maps in normal mode to init the node/scope selection
+          node_incremental = "grn", -- increment to the upper named parent
+          scope_incremental = "grc", -- increment to the upper scope (as defined in locals.scm)
+          node_decremental = "grm", -- decrement to the previous node
         },
       },
       textobjects = {
+        lsp_interop = {
+          enable = true,
+          peek_definition_code = {
+            ["DF"] = "@function.outer",
+            -- ["DF"] = "@class.outer",
+          },
+        },
         move = {
           enable = true,
-          goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
-          goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
-          goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
-          goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+          goto_next_start = {
+            ["]f"] = "@function.outer",
+            ["]c"] = "@class.outer",
+            -- ["]i"] = "@conditional.outer",
+            -- ["]a"] = "@block.outer",
+          },
+          goto_next_end = {
+            ["]F"] = "@function.outer",
+            ["]C"] = "@class.outer",
+            -- ["]I"] = "@conditional.outer",
+            -- ["]A"] = "@block.outer",
+          },
+          goto_previous_start = {
+            ["[f"] = "@function.outer",
+            ["[c"] = "@class.outer",
+            -- ["[i"] = "@conditional.outer",
+            -- ["[a"] = "@block.outer",
+          },
+          goto_previous_end = {
+            ["[F"] = "@function.outer",
+            ["[C"] = "@class.outer",
+            -- ["[I"] = "@conditional.outer",
+            -- ["[A"] = "@block.outer",
+          },
+        },
+        select = {
+          enable = true,
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+          },
         },
       },
     },
@@ -111,4 +151,3 @@ return {
     end,
   },
 }
-
